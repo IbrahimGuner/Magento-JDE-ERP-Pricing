cf4a856ef61a982aa4365432c1abbfa68da5ae74
magepoet <gnudrivers@gmail.com>
2021-03-30 08:57:29 -0700

    Update registration.php

--- app/code/Atrium/CatalogRuleApi/registration.php
+++ app/code/Atrium/CatalogRuleApi/registration.php
@@ -1,6 +1,6 @@
 <?php
     \Magento\Framework\Component\ComponentRegistrar::register(
         \Magento\Framework\Component\ComponentRegistrar::MODULE,
-        'CatalogRuleApi',
+        'Atrium+CatalogRuleApi',
         __DIR__
     );
